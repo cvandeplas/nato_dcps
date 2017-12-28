c3194ab7cb908fe2391104a609cfa17a7dba6b0f
Christophe Vandeplas <christophe@vandeplas.com>
2017-12-28 20:28:29 +0100

    website scraping as db_ function

--- dcps.py
+++ dcps.py
@@ -63,128 +63,183 @@ def date_to_unix(s):
     return time.mktime(datetime.strptime(s, "%d/%m/%Y").timetuple())
 
 
-# build a permanent session object, this way we keep all cookies and such
-s = requests.Session()
+def db_update_from_online():
+    # build a permanent session object, this way we keep all cookies and such
+    s = requests.Session()
 
-# first login on the main portal
-payload = {'id': dcps_id, 'pw': dcps_pwd, 'submit': 'SIGN ON'}
-r = s.post(dcps_url, data=payload)
+    # first login on the main portal
+    payload = {'id': dcps_id, 'pw': dcps_pwd, 'submit': 'SIGN ON'}
+    r = s.post(dcps_url, data=payload)
 
-# find the details of the form submit to login on the sub-site
-soup = BeautifulSoup(r.text, 'lxml')
-i = soup.find('input', attrs={"name": "token-authentication"})
-if not i:
-    exit("ERROR: Authentication error, cannot login.")
-payload = {'token-authentication': i.get('value'), 'ecol': 'Go To My Dcps'}
-url = i.parent.get('action')  # load the URL from the form, this way we don't expose it here
-r = s.post(url, data=payload)
-# now we are on the real site with the numbers
+    # find the details of the form submit to login on the sub-site
+    soup = BeautifulSoup(r.text, 'lxml')
+    i = soup.find('input', attrs={"name": "token-authentication"})
+    if not i:
+        exit("ERROR: Authentication error, cannot login.")
+    payload = {'token-authentication': i.get('value'), 'ecol': 'Go To My Dcps'}
+    url = i.parent.get('action')  # load the URL from the form, this way we don't expose it here
+    r = s.post(url, data=payload)
+    # now we are on the real site with the numbers
 
-#
-# load the MY CONTRIBUTION BALANCE page containing all the juicy details
-#
-if "Your TEMPORARY first-access password" in r.text:
-    exit("ERROR: Change of password requested. Please login manually and change your password.")
-soup = BeautifulSoup(r.text, 'lxml')
-tmp_input = soup.find('input', value="MAIN-APP-I-I-IOM")
-url = '/'.join(r.url.split('/')[:3]) + tmp_input.parent.get('action')  # load the URL from within the page, this way we don't expose it here
-payload = {'f-token': 'MAIN-APP-I-I-IOM',
-           'c-token': 'MAIN-APP-I-I-IWP-WEP',
-           'a-token': 'null'}
-r = s.post(url, payload)
-soup = BeautifulSoup(r.text, 'lxml')
+    #
+    # load the MY CONTRIBUTION BALANCE page containing all the juicy details
+    #
+    if "Your TEMPORARY first-access password" in r.text:
+        exit("ERROR: Change of password requested. Please login manually and change your password.")
+    soup = BeautifulSoup(r.text, 'lxml')
+    tmp_input = soup.find('input', value="MAIN-APP-I-I-IOM")
+    url = '/'.join(r.url.split('/')[:3]) + tmp_input.parent.get('action')  # load the URL from within the page, this way we don't expose it here
+    payload = {'f-token': 'MAIN-APP-I-I-IOM',
+               'c-token': 'MAIN-APP-I-I-IWP-WEP',
+               'a-token': 'null'}
+    r = s.post(url, payload)
+    soup = BeautifulSoup(r.text, 'lxml')
 
-sql_conn = sqlite3_createdb()
+    sql_conn = sqlite3_createdb()
 
-
-# Balance Previous year
-tmp_balance_at_tds = soup.find_all('td', limit=2, string=re.compile("Balance at"))
-balance_year_table = tmp_balance_at_tds[0].parent.parent
-results = normalise_data(table_to_dict_array(balance_year_table))
-print()
-print("BALANCE PREVIOUS YEAR")
-print(tabulate(results, headers='keys'))
-c = sql_conn.cursor()
-for i in results:
-    c.execute("INSERT INTO balance_year VALUES(?,?,?,?,?,?,?)", [
-        i['NAV date'],
-        date_to_unix(i['NAV date']),
-        i['Currency'],
-        i['Fund'],
-        i['Amount'],
-        i['Total Units'],
-        i['Price per UNIT']])
-sql_conn.commit()
-
-
-# Current Year contributions - Summary
-tmp_year_details_td = soup.find('td', string=re.compile("Current Year Details"))
-year_details_table = tmp_year_details_td.parent.parent
-results = normalise_data(table_to_dict_array(year_details_table))
-print()
-print("CURRENT YEAR CONTRIBUTIONS - SUMMARY")
-print(tabulate(results, headers='keys'))
-c = sql_conn.cursor()
-for i in results:
-    c.execute("INSERT INTO contributions VALUES(?,?,?,?,?)", [
-        i['Reference Date'],
-        date_to_unix(i['Reference Date']),
-        i['Currency'],
-        i['Operation Code'],
-        i['Total Amount']])
-sql_conn.commit()
-
-# Current Year contributions - Detail
-print()
-print("CURRENT YEAR CONTRIBUTIONS - DETAILS")
-ahrefs = year_details_table.find_all('a')
-urls = set()
-for ahref in ahrefs:
-    urls.add(ahref.get('href'))
-j = 0
-for url in urls:
-    url = '/'.join(r.url.split('/')[:3]) + url  # load the URL from within the page, this way we don't expose it here
-    url_r = s.get(url)
-    j += 1
-    url_soup = BeautifulSoup(url_r.text, 'lxml')
-    tmp_operation_date_td = url_soup.find('th', string=re.compile("Operation Date"))
-    url_details_table = tmp_operation_date_td.find_parent('table')
-    url_results = normalise_data(table_to_dict_array(url_details_table))
+    # Balance Previous year
+    tmp_balance_at_tds = soup.find_all('td', limit=2, string=re.compile("Balance at"))
+    balance_year_table = tmp_balance_at_tds[0].parent.parent
+    results = normalise_data(table_to_dict_array(balance_year_table))
     print()
-    print(tabulate(url_results, headers='keys'))
+    print("BALANCE PREVIOUS YEAR")
+    print(tabulate(results, headers='keys'))
     c = sql_conn.cursor()
-    for i in url_results:
-        if len(i) == 0:
-            continue
-        c.execute("INSERT INTO contributions_detail VALUES(?,?,?,?,?,?,?,?,?,?,?)", [
-            i['Operation Date'],
-            date_to_unix(i['Operation Date']),
-            i['Nav Date'],
-            date_to_unix(i['Nav Date']),
+    for i in results:
+        c.execute("INSERT INTO balance_year VALUES(?,?,?,?,?,?,?)", [
+            i['NAV date'],
+            date_to_unix(i['NAV date']),
+            i['Currency'],
             i['Fund'],
-            i['Exchange Rate'],
-            i['Gross Amount Inv/Dis'],
-            i['Fees (*)'],
-            i['Net Amount Inv/Dis'],
-            i['No. of Units'],
-            i['Price per Unit']])
+            i['Amount'],
+            i['Total Units'],
+            i['Price per UNIT']])
+    sql_conn.commit()
+
+    # Current Year contributions - Summary
+    tmp_year_details_td = soup.find('td', string=re.compile("Current Year Details"))
+    year_details_table = tmp_year_details_td.parent.parent
+    results = normalise_data(table_to_dict_array(year_details_table))
+    print()
+    print("CURRENT YEAR CONTRIBUTIONS - SUMMARY")
+    print(tabulate(results, headers='keys'))
+    c = sql_conn.cursor()
+    for i in results:
+        c.execute("INSERT INTO contributions VALUES(?,?,?,?,?)", [
+            i['Reference Date'],
+            date_to_unix(i['Reference Date']),
+            i['Currency'],
+            i['Operation Code'],
+            i['Total Amount']])
+    sql_conn.commit()
+
+    # Current Year contributions - Detail
+    print()
+    print("CURRENT YEAR CONTRIBUTIONS - DETAILS")
+    ahrefs = year_details_table.find_all('a')
+    urls = set()
+    for ahref in ahrefs:
+        urls.add(ahref.get('href'))
+    j = 0
+    for url in urls:
+        url = '/'.join(r.url.split('/')[:3]) + url  # load the URL from within the page, this way we don't expose it here
+        url_r = s.get(url)
+        j += 1
+        url_soup = BeautifulSoup(url_r.text, 'lxml')
+        tmp_operation_date_td = url_soup.find('th', string=re.compile("Operation Date"))
+        url_details_table = tmp_operation_date_td.find_parent('table')
+        url_results = normalise_data(table_to_dict_array(url_details_table))
+        print()
+        print(tabulate(url_results, headers='keys'))
+        c = sql_conn.cursor()
+        for i in url_results:
+            if len(i) == 0:
+                continue
+            c.execute("INSERT INTO contributions_detail VALUES(?,?,?,?,?,?,?,?,?,?,?)", [
+                i['Operation Date'],
+                date_to_unix(i['Operation Date']),
+                i['Nav Date'],
+                date_to_unix(i['Nav Date']),
+                i['Fund'],
+                i['Exchange Rate'],
+                i['Gross Amount Inv/Dis'],
+                i['Fees (*)'],
+                i['Net Amount Inv/Dis'],
+                i['No. of Units'],
+                i['Price per Unit']])
+        sql_conn.commit()
+
+    # Current Balance
+    balance_now_table = tmp_balance_at_tds[1].parent.parent
+    results = normalise_data(table_to_dict_array(balance_now_table))
+    print()
+    print("CURRENT BALANCE")
+    print(tabulate(results, headers='keys'))
+    c = sql_conn.cursor()
+    for i in results:
+        c.execute("INSERT INTO balance_now VALUES(?,?,?,?,?,?,?)", [
+            i['NAV date'],
+            date_to_unix(i['NAV date']),
+            i['Currency'],
+            i['Fund'],
+            i['Amount'],
+            i['Total Units'],
+            i['Price per UNIT']])
     sql_conn.commit()
 
 
-# Current Balance
-balance_now_table = tmp_balance_at_tds[1].parent.parent
-results = normalise_data(table_to_dict_array(balance_now_table))
-print()
-print("CURRENT BALANCE")
-print(tabulate(results, headers='keys'))
-c = sql_conn.cursor()
-for i in results:
-    c.execute("INSERT INTO balance_now VALUES(?,?,?,?,?,?,?)", [
-        i['NAV date'],
-        date_to_unix(i['NAV date']),
-        i['Currency'],
-        i['Fund'],
-        i['Amount'],
-        i['Total Units'],
-        i['Price per UNIT']])
-sql_conn.commit()
+def db_get_funds():
+    '''
+    returns a list of the funds
+    '''
+    sql_conn = sqlite3_createdb()
+    c = sql_conn.cursor()
+    c.execute("SELECT DISTINCT fund FROM balance_now ORDER BY fund")
+    result = c.fetchall()
+    return [x[0] for x in result]
+
+
+# JUST PLAYING AROUND
+def db_get_contributions_sum():
+    sql_conn = sqlite3_createdb()
+    c = sql_conn.cursor()
+    c.execute("SELECT SUM(amount) FROM contributions")
+    result = c.fetchall()
+    return round(result[0][0], 2)
+
+
+def db_get_latest_balance():
+    '''
+    get the sum of the funds at the last balance
+    '''
+    sql_conn = sqlite3_createdb()
+    c = sql_conn.cursor()
+    c.execute("SELECT amount FROM balance_now ORDER BY date_unix DESC LIMIT (SELECT COUNT (DISTINCT fund) FROM balance_now)")
+    result = c.fetchall()
+    return sum([x[0] for x in result])
+    # return round(result[0][0], 2)
+
+
+# db_update_from_online()
+
+# FIXME add command line parsing and help
+# -q -- quiet mode, output nothing except errors. great as cronjob
+# -d <> -- debug 1 to 9
+# --first-run or --magic -- first run, do magic: extract data, extract old data from Individual Statement PDFs and compute data based on historical fund value
+# -f -- path for database, (default dcps.sqlite3.db)
+
+
+
+# TESTING
+# sql_conn = sqlite3_createdb()
+# c = sql_conn.cursor()
+
+# c.execute("SELECT * FROM contributions_detail ORDER BY date_nav_unix DESC")
+# print(c.fetchone())
+
+# print(db_get_funds())
+
+# print(db_get_contributions_sum())
+
+# print(db_get_latest_balance())
+
